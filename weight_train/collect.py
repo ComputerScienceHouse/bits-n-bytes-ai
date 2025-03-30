a2b11f4e0b6eac5d15277e926a2f942fe9ce4950
IsaacIngram <me@isaacingram.dev>
2025-03-29 22:43:11 -0400

    Fix MQTT, write to tmp CSVs

--- weight_train/collect.py
+++ weight_train/collect.py
@@ -8,6 +8,7 @@
 #
 ###############################################################################
 import datetime
+import os
 import time
 from os import environ
 import sys
@@ -22,12 +23,16 @@ import csv
 MQTT_URL = environ.get('MQTT_URL', 'test.mosquitto.org')
 MQTT_PORT = environ.get('MQTT_PORT', 1883)
 MQTT_DATA_TOPIC = 'shelf/data'
-TMP_MQTT_DATA_PATH = Path('/tmp/mqtt.csv')
-TIME_STR_FT = '%H:%M%S-%d-%m-%Y'
+TMP_MQTT_DATA_PATH = Path.cwd() / Path('tmp/mqtt-data.csv')
+TMP_INTERACTION_DATA_PATH = Path.cwd() / Path('tmp/interaction-data.csv')
+TIME_STR_FT = '%H:%M:%S-%d-%m-%Y'
 
 # Global variables
-shelf_id = ""
-slot_id = 0
+shelf_id = environ.get('SHELF_ID', "")
+try:
+    slot_id = int(environ.get('SLOT_ID', 0))
+except TypeError:
+    slot_id = 0
 window_ms = 5000
 
 
@@ -52,6 +57,7 @@ def on_msg_callback(client, userdata, msg):
     global shelf_id
     global window_ms
     global slot_id
+    print("Message received")
     # Get the time that this message was received
     current_time = datetime.datetime.now().strftime(TIME_STR_FT)
     # Load message as JSON
@@ -71,8 +77,6 @@ def on_msg_callback(client, userdata, msg):
                     csv_writer = csv.writer(out_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
                     csv_writer.writerow([current_time, current_weight])
 
-def clear_line():
-    print("\033[K", end='')
 
 def main():
 
@@ -80,20 +84,30 @@ def main():
     global window_ms
     global slot_id
 
+    # Connect to MQTT, subscribe to data topic, and start MQTT thread
     mqtt = mqtt_client.Client(mqtt_enums.CallbackAPIVersion.VERSION2)
     mqtt.connect(MQTT_URL, MQTT_PORT)
-    mqtt.subscribe(MQTT_DATA_TOPIC)
+    mqtt.subscribe(MQTT_DATA_TOPIC, qos=0)
     mqtt.message_callback_add(MQTT_DATA_TOPIC, on_msg_callback)
-
-    if len(sys.argv) == 2:
-        output_csv_path = Path(sys.argv[1])
-    else:
-        output_csv_path = Path(input("Enter output CSV path:"))
+    mqtt.loop_start()
 
 
+    # Create mqtt data csv if it doesn't exist
+    if not TMP_MQTT_DATA_PATH.exists():
+        os.makedirs(os.path.dirname(TMP_MQTT_DATA_PATH), exist_ok=True)
+        with open(TMP_MQTT_DATA_PATH, 'w') as file:
+            pass
+
+    # Create interaction data csv if it doesn't exist
+    if not TMP_INTERACTION_DATA_PATH.exists():
+        os.makedirs(os.path.dirname(TMP_INTERACTION_DATA_PATH), exist_ok=True)
+        with open(TMP_INTERACTION_DATA_PATH, 'w') as file:
+            pass
+
+    # Print user instructions
     print("Welcome to weight_train collect. Type 'help' for a list of commands or 'exit' to exit.")
-    print("Important Note: If you exit without using the 'exit' command, the data gathered will not be compiled into usable data!")
 
+    # Main CLI loop
     while True:
         command = input('>').split()
         match command:
@@ -112,6 +126,7 @@ def main():
                 except ValueError or IndexError:
                     print("Invalid arguments")
             case ['record', *arguments]:
+                current_time = datetime.datetime.now().strftime(TIME_STR_FT)
                 if len(arguments) != 1:
                     print("Unknown number of args")
                 else:
@@ -120,16 +135,32 @@ def main():
                         num_items = int(arguments[0])
                     except TypeError:
                         print("Invalid number")
+                    # Write instruction message to terminal
                     if num_items > 0:
                         print("Remove %d items from slot %d..." % (num_items, slot_id))
                     elif num_items < 0:
                         print("Add %d items to slot %d..." % (-1 * num_items, slot_id))
                     else:
                         print("Do not touch items...")
+                    # Pause however many seconds
                     start_time_ms = time.time() * 1000
                     while time.time() * 1000 < start_time_ms + window_ms:
                         pass
+                    # Write output to terminal
+                    with open(TMP_INTERACTION_DATA_PATH, 'a') as out_file:
+                        csv_writer = csv.writer(out_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                        csv_writer.writerow([current_time, num_items])
                     print("Recorded. If you did not complete instruction, type 'delete' to remove the last datapoint.")
+            case ['delete']:
+                with open(TMP_INTERACTION_DATA_PATH, 'r') as in_file:
+                    csv_reader = csv.reader(in_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    rows = list()
+                    for row in csv_reader:
+                        rows.append(row)
+                with open(TMP_INTERACTION_DATA_PATH, 'w') as out_file:
+                    csv_writer = csv.writer(out_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    csv_writer.writerows(rows[:-1])
+                print("Deleted last datapoint.")
             case _:
                 print("Unknown command, type 'help' for a list of commands.")
 
